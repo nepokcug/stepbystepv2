c1a68cabc60f508a82668a8c67d28fe632dc18de
Vilmir Yakupov <pirogolik@yandex.ru>
2026-05-30 20:29:48 +0300

    first commit

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,34 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
 	// TODO: добавить методы
+	Parse(datastring string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
+	//Прокручиваем Dataset
+	for i, data := range dataset {
+		err := dp.Parse(data)
+		if err != nil {
+			log.Printf("Ошибка парсинга строки %d (%s): %v\n", i+1, data, err)
+			continue
+		}
+
+		//Формируем и выводим строку с информацией об активности
+		info, err := dp.ActionInfo()
+		if err != nil {
+			// Ошибка при формировании информации - логируем
+			log.Printf("Ошибка получения информации для строки %d (%s): %v\n", i+1, data, err)
+			continue
+		}
+		fmt.Println(info)
+		fmt.Println()
+	}
 }
--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -1,9 +1,17 @@
 package personaldata
 
+import "fmt"
+
 type Personal struct {
 	// TODO: добавить поля
+	Name   string
+	Weight float64
+	Height float64
 }
 
 func (p Personal) Print() {
 	// TODO: реализовать функцию
+	fmt.Println("Имя:", p.Name)
+	fmt.Println("Вес:", p.Weight)
+	fmt.Println("Рост:", p.Height)
 }
